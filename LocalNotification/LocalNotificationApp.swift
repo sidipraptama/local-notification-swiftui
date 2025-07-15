cb3ffd7c8440decacd8a22a2fbb4c67e7086efa9
sidipraptama <sidiaurelius@gmail.com>
2025-07-15 21:14:56 +0700

    Initial Commit

---
+++ LocalNotification/LocalNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationApp.swift
+//  LocalNotification
+//
+//  Created by Sidi Praptama Aurelius Nurhalim on 15/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocalNotificationTests/LocalNotificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationTests.swift
+//  LocalNotificationTests
+//
+//  Created by Sidi Praptama Aurelius Nurhalim on 15/07/25.
+//
+
+import Testing
+@testable import LocalNotification
+
+struct LocalNotificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
